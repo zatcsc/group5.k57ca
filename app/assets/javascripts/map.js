ba428468618a40792018765033435bee7578b97d
thinhnd_57 <thinhnd_57@vnu.edu.vn>
2014-04-27 20:59:56 +0700

    Showed some sample jam data
    Some sample jam places show on the map, can't be turned off at the
    moment.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,68 +1,74 @@
 var map;
 function initialize() {
-  var mapOptions = {
-    center: new google.maps.LatLng(21.011111, 105.85),
-    mapTypeId : google.maps.MapTypeId.ROADMAP,
-    zoom: 13
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-    mapOptions);
+	var mapOptions = {
+		center : new google.maps.LatLng(21.011111, 105.85),
+		mapTypeId : google.maps.MapTypeId.ROADMAP,
+		zoom : 13
+	};
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  var input = /** @type {HTMLInputElement} */(
-      document.getElementById('pac-input'));
-  var startInput = document.getElementById("start-place");
-  var endInput = document.getElementById("end-place");
+	var input = /** @type {HTMLInputElement} */(
+		document.getElementById('pac-input'));
+	var startInput = document.getElementById("start-place");
+	var endInput = document.getElementById("end-place");
 
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  
-  autocomplete.bindTo('bounds', map);
+	var autocomplete = new google.maps.places.Autocomplete(input);
 
-  var infowindow = new google.maps.InfoWindow();
-  var marker = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
-  });
+	autocomplete.bindTo('bounds', map);
 
-  google.maps.event.addListener(autocomplete, 'place_changed', function() {
-    infowindow.close();
-    marker.setVisible(false);
-    var place = autocomplete.getPlace();
-    if (!place.geometry) {
-      return;
-    }
+	var infowindow = new google.maps.InfoWindow();
+	var marker = new google.maps.Marker({
+		map : map,
+		anchorPoint : new google.maps.Point(0, -29)
+	});
 
-    // If the place has a geometry, then present it on a map.
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);      
-      map.setZoom(17);  // Why 17? Because it looks good.
-      console.log(place.geometry.location);
-    }
-    marker.setIcon(/** @type {google.maps.Icon} */({
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(35, 35)
-    }));
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
+	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+		infowindow.close();
+		marker.setVisible(false);
+		var place = autocomplete.getPlace();
+		if (!place.geometry) {
+			return;
+		}
 
-    var address = '';
-    if (place.address_components) {
-      address = [
-        (place.address_components[0] && place.address_components[0].short_name || ''),
-        (place.address_components[1] && place.address_components[1].short_name || ''),
-        (place.address_components[2] && place.address_components[2].short_name || '')
-      ].join(' ');
-    }
+		// If the place has a geometry, then present it on a map.
+		if (place.geometry.viewport) {
+			map.fitBounds(place.geometry.viewport);
+		} else {
+			map.setCenter(place.geometry.location);
+			map.setZoom(17);
+			// Why 17? Because it looks good.
+			console.log(place.geometry.location);
+		}
+		marker.setIcon(/** @type {google.maps.Icon} */( {
+			url : place.icon,
+			size : new google.maps.Size(71, 71),
+			origin : new google.maps.Point(0, 0),
+			anchor : new google.maps.Point(17, 34),
+			scaledSize : new google.maps.Size(35, 35)
+		}));
+		marker.setPosition(place.geometry.location);
+		marker.setVisible(true);
 
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    infowindow.open(map, marker);
-  });
+		var address = '';
+		if (place.address_components) {
+			address = [(place.address_components[0] && place.address_components[0].short_name || ''), (place.address_components[1] && place.address_components[1].short_name || ''), (place.address_components[2] && place.address_components[2].short_name || '')].join(' ');
+		}
 
-  // Autocomplete.
-  autocomplete.setTypes([]);
-      
+		infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+		infowindow.open(map, marker);
+	});
+
+	// Autocomplete.
+	autocomplete.setTypes([]);
+
+}
+
+function putMarker(myLat, myLng) {
+	//Marker test
+	var JamMarker = new google.maps.Marker({
+		position : new google.maps.LatLng(myLat, myLng),
+		map : map,
+		title : "Tắc ở đây :v",
+		icon : "/assets/jam1.png"
+	});
 }
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,25 +1,26 @@
 class WelcomeController < ApplicationController
   def about
-  	render "about"
+    render "about"
   end
-  
-  def map    
-  	render "map"
+
+  def map
+    @jam_places = Jam.all
+    render "map"
   end
 
   def routedirection
-  	render "routedirection"
+    render "routedirection"
   end
-  
+
   def submitData
     @jam_places = Jam.all
   end
-  
+
   def add
     new_jam = Jam.create(:latitude => params[:jam_lat], :longtitude => params[:jam_lgn])
     if !new_jam.valid?
       flash[:error] = new_jam.errors.full_messages.join(" ").html_safe
-    end  
+    end
     redirect_to :action => 'submitData'
   end
 
