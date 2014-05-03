0aa8ec18064141c546025335e8ea49eaa841739e
Nguyen Thac Thong <nguyenthacthong@gmail.com>
2014-05-03 08:12:40 +0700

    create test for sign-in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do  
   resources :users
+  resources :sessions, :only => [:new,:create,:destroy]
   match '/signup',    to:"users#new",        via: 'get'
   match '/about',     to: "welcome#about",   via: 'get'
   match '/contact',   to: 'welcome#contact', via: 'get'
   match '/home',      to: 'welcome#map',     via: 'get'
-
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout',to: 'sessions#destroy', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
