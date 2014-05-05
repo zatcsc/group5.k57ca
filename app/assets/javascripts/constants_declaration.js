15c2c07e029d33786541ef967857403860e40bff
Nguyen Thac Thong <nguyenthacthong@gmail.com>
2014-05-05 10:48:43 +0700

    refactor code && fix javascript eror

---
+++ app/assets/javascripts/constants_declaration.js
@@ -0,0 +1,13 @@
+/* Constants declaration*/
+var HANOI_LONGITUDE = 105.85;
+var HANOI_LATITUDE = 21.011111;
+var GOOD_ZOOM_LEVEL  = 17;
+var MAP_ICON_SIZE = new google.maps.Size(71, 71);
+var SCALED_MAP_ICON_SIZE = new google.maps.Size(35, 35);
+var HANOI_CENTER = new google.maps.LatLng(HANOI_LATITUDE,HANOI_LONGITUDE);
+var ROADMAP = google.maps.MapTypeId.ROADMAP;
+var GOOD_MARKER_ANCHOR_POINT = new google.maps.Point(0,-29);
+var VEHICLE_TYPES= new Array("car","bus","bike","walk");
+var TRAVEL_MODES = new Array("DRIVING","TRANSIT","BICYCLING","WALKING")	
+var FEATURES = new Array("find-route","traffic-information","weather","traffic-statistic");
+/* End constants */
