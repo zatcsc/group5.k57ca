b27d9576958129d83284f1c4021b4793dc1de3fa
Nguyen Thac Thong <nguyenthacthong@gmail.com>
2014-05-07 07:58:44 +0700

    added weather model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506153019) do
+ActiveRecord::Schema.define(version: 20140507004800) do
 
   create_table "coordinates", force: true do |t|
     t.datetime "created_at"
@@ -31,5 +31,15 @@ ActiveRecord::Schema.define(version: 20140506153019) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+
+  create_table "weathers", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "day"
+    t.float    "min_temperature"
+    t.float    "max_temperature"
+    t.string   "status"
+    t.date     "current_date"
+  end
 
 end
