cc030b92568236e1e7557c99d2670408fe96c39b
Nguyen Thac Thong <nguyenthacthong@gmail.com>
2014-05-07 08:59:33 +0700

    fixed test error

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,7 +9,7 @@ describe "AuthenticationPages" do
   	describe "with valid information" do
   		let(:user) { FactoryGirl.create(:user) }
   		before do        
-  		  fill_in "Email", :with => user.email.upcase
+  		  fill_in "Email", :with => user.email
   		  fill_in "Password", :with => user.password
   		  click_button "Sign in"
   		end
