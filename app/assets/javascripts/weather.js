3554462cd33cbfe464a8a1b6370fb93c65685c68
Nguyen Thac Thong <nguyenthacthong@gmail.com>
2014-05-22 16:34:35 +0700

    fix #14 issue with weather function

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -1,18 +1,20 @@
+var getWeatherInfo = function(event) {
+	/* Act on the event */
+	var weather_value = $(".weather-select-box option:selected").val();
+	// alert(weather_value);
+	$.ajax({
+		type:"POST",
+		url: "/weather",
+		data: { city_id: weather_value}
+	})
+	.done(function( html ){
+		$('.current-weather').html("");
+		$('.current-weather').html(html);
+	});		
+ };
 var weatherReady = function(){
-	$(".weather-select-box").change(function(event) {
-		/* Act on the event */
-		var weather_value = $(".weather-select-box option:selected").val();
-		// alert(weather_value);
-		$.ajax({
-			type:"POST",
-			url: "/weather",
-			data: { city_id: weather_value}
-		})
-		.done(function( html ){
-			$('.current-weather').html("");
-			$('.current-weather').html(html);
-		});		
-    });	
+	$(".weather-select-box").change(getWeatherInfo);	
+	$('.weather').on('click',getWeatherInfo);   
 };
 $(document).ready(weatherReady) ;
 $(document).on('page:load',weatherReady);
